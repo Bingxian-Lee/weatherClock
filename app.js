f3e4eac3279c39542b382a75f8a353b190228ba0
bing xian lee <bingxian.lee@gmail.com>
2021-06-14 16:49:44 +0800

    Add README.md and weather app

--- app.js
+++ app.js
@@ -1,7 +1,76 @@
+//Select Elements
 const hour = document.querySelector(".hr");
 const minute = document.querySelector(".min");
 const second = document.querySelector(".sec");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
 
+// weather app units
+const weather = {};
+
+weather.temperature = {
+    unit: "celsius"
+}
+
+// weather const and vars
+const KELVIN = 273;
+
+// API KEY
+const key = "bf5b92f536d0a0e17eb17cfc44f2857c";
+
+// CHECK IF BROWSER SUPPORTS GEOLOCATION
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+
+// SET USER'S POSITION
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    
+    getWeather(latitude, longitude);
+}
+
+// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+// GET WEATHER FROM API PROVIDER
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+
+// DISPLAY WEATHER TO UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
 
 //digital clock functionality
 function clock(){
@@ -27,7 +96,7 @@ function clock(){
 
 }
 
-// analog clock functionality
+//analog clock functionality
 function analog(){
     const fullDate = new Date();
     var hours = fullDate.getHours();
@@ -46,5 +115,3 @@ function analog(){
 // updates hours minute seconds every 1 miliseconds
 setInterval(clock,100);
 setInterval(analog,100);
-
-
